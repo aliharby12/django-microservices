7740e2c87f487125cbde77f3d2eee18f5364c3b5
ali <ali@sonono.ch>
2022-04-26 00:47:28 +0200

    connect every service with postgresql database and edit admin files

--- votes/votes/settings.py
+++ votes/votes/settings.py
@@ -80,13 +80,17 @@ WSGI_APPLICATION = 'votes.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'vote_db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
